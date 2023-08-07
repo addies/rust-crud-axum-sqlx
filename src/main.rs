393979fd7e0d7b2e38845a1f187f06c252eaa88d
Addiestar Silaban <addiestars@gmail.com>
2023-08-07 14:25:22 +0700

    Add files via upload
    main file crud axum sqlx postgres

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use axum::{
+    extract::{Path, State},
+    routing::{get, post},
+    Router,
+    http::StatusCode,
+    response::IntoResponse,
+    Json
+};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use tower_http::cors::{Any, CorsLayer};
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+
+
+#[derive(Serialize, Deserialize, sqlx::FromRow)]
+#[allow(non_snake_case)]
+pub struct MyTable {
+    nomer: i64,
+    nama: String,
+    alamat: String,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres,and Axum";
+    let json_response = serde_json::json!({
+        "status": "success",
+        "message": MESSAGE
+    });
+
+    Json(json_response)
+}
+
+async fn getall_mytable (State(pool):State<PgPool>) 
+    -> Result<Json<Vec<MyTable>>, (StatusCode, String)> {
+    let string_query = "SELECT * FROM mytable";
+    let result = sqlx::query_as(string_query)
+        .fetch_all(&pool)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, format!("Error is: {}", err)))?;
+
+    Ok(Json(result))    
+}
+
+async fn get_mytable (State(pool):State<PgPool>, Path(nomer): Path<i64>) 
+    -> Result<Json<Vec<MyTable>>, (StatusCode, String)> {
+    let string_query = "SELECT * FROM mytable WHERE nomer = $1";
+    let result = sqlx::query_as(string_query)
+        .bind(nomer)
+        .fetch_all(&pool)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, format!("Error is: {}", err)))?;
+
+    Ok(Json(result))   
+}
+
+pub async fn create_mytable( State(pool):State<PgPool>, Json(my_table):Json<MyTable>) 
+    -> Result<Json<Value>, (StatusCode, String)> {
+    let string_query = "INSERT INTO mytable (nomer,nama,alamat) VALUES ($1, $2, $3)";
+    let _result = sqlx::query(string_query)
+        .bind(&my_table.nomer)
+        .bind(&my_table.nama)
+        .bind(&my_table.alamat)
+        .execute(&pool)
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, format!("Error is: {}", err)))?;
+
+    Ok(Json(json!(my_table))) 
+}
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    let cors: CorsLayer = CorsLayer::new()
+        .allow_origin(Any);
+
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url) 
+        .await
+        {
+            Ok(pool) => {
+                println!("✅Connection to the database is successful!");
+                pool
+            }
+            Err(err) => {
+                println!("🔥 Failed to connect to the database: {:?}", err);
+                std::process::exit(1);
+            }
+        };
+
+    println!("🚀 Server started successfully");
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(health_checker_handler))
+        .route("/health_checker_handler", get(health_checker_handler))
+        .route("/api/mytable", get(getall_mytable))
+        .route("/api/mytable/:nomer", get(get_mytable))
+        .route("/api/mytable", post(create_mytable))
+        .with_state(pool)
+        .layer(cors);
+    
+    println!("Listening on port {}", "0.0.0.0:3000");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
